ead2003ccac8780b58e380ba7de65860ad5e5f26
Jonathan Cubides <prieto.jona@gmail.com>
2026-08-06 22:50:45 -0500

    chore(release): set version 0.1.9

--- lakefile.lean
+++ lakefile.lean
@@ -2,7 +2,7 @@ import Lake
 open Lake DSL
 
 package «termcolor-widgets» where
-  version := v!"0.1.8"
+  version := v!"0.1.9"
   leanOptions := #[⟨`autoImplicit, false⟩, ⟨`relaxedAutoImplicit, false⟩]
 
 require «termcolor-layout» from git
