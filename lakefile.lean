f99944ee1e74280f21d1dc9b9ab6b7a1cb14c084
Jonathan Cubides <prieto.jona@gmail.com>
2026-09-08 22:31:09 -0500

    build: refresh termcolor stack dependencies

--- lakefile.lean
+++ lakefile.lean
@@ -7,11 +7,11 @@ package «termcolor-widgets» where
 
 require «termcolor-layout» from git
   "https://github.com/jonaprieto/lean-termcolor-layout.git"
-  @ "v0.1.13"
+  @ "v0.1.14"
 
 require «termcolor» from git
   "https://github.com/jonaprieto/lean-termcolor.git"
-  @ "v1.1.5"
+  @ "v1.1.6"
 
 @[default_target]
 lean_lib «TermColor.Widgets» where
